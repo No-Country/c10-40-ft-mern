599e8341237dcff0de4be0ac6c776aefdda2fa9a
Yako <dyakobowicz@gmail.com>
2023-03-27 16:12:14 -0300

    fix: test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App(): React.ReactNode {
   return (
     <div className="App">
-      <h1 className="text-5xl font-bold">Workout tracker </h1>
+      <h1 className="text-5xl font-bold">Workout tracker</h1>
     </div>
   )
 }
