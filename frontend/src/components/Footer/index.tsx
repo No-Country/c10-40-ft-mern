e615d3d5e90afe0ac211f495b3cf4c340cdb52bb
arbelais <arbelaism@outlook.com>
2023-03-28 20:04:01 -0300

    feat: navbar and footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router-dom'
+
 const Footer = (): JSX.Element => {
-  return <div>Footer</div>
+  return (
+    <footer className="w-full border border-red-500">
+      <div>
+        <ul>
+          <li className="underline text-blue-400 cursor-pointer">
+            <Link to={'/'}>Home</Link>
+          </li>
+          <li className="underline text-blue-400 cursor-pointer">
+            <Link to={'/about'}>About</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full py-4 text-center">
+        <p>
+          &#169; {new Date().getFullYear()}.<span> Workout Tracker</span>
+        </p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
