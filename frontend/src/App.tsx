e3bfd2bcdd614178802f0d2210cf0b70030e0c88
arbelais <arbelaism@outlook.com>
2023-03-28 17:22:31 -0300

    feat: add react-router. fix eslint error. add vite-tsconfig-path. folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import { Home, About } from 'pages'
+import { Route, Routes } from 'react-router-dom'
+
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <h1 className="text-7xl font-bold">Workout tracker </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from 'components'
+
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-7xl font-bold">Workout tracker </h1>
+    </div>
+  )
+}
+
+export default Home
