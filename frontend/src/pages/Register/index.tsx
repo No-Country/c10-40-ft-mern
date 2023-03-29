2a61486fe686b61aeecf2527dd14949b8600a00a
Yako <dyakobowicz@gmail.com>
2023-03-29 19:55:27 -0300

    style: :art: added floating label to complete profile and login

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -34,7 +34,7 @@ const Register = (): JSX.Element => {
           </div>
           <div className="relative">
             <input
-              type="text"
+              type="mail"
               id="email"
               className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
               placeholder=" "
