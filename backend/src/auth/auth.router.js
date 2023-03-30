8024a30d7d94a095ce47f8ae8ee51b8972031d8a
yonroa <yonanderson11@gmail.com>
2023-03-29 22:00:08 -0500

    feat: Se agrega la funcionalidad de iniciar sesion con google (Aun con errores)

--- backend/src/auth/auth.router.js
+++ backend/src/auth/auth.router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 
+const passport = require('passport');
 const postLogin = require('./auth.services')
 
 router.post('/login', postLogin)
+
+router.get("/google", passport.authenticate('auth-google', { scope: ["email", "profile"] })
+);
+
+router.get("/oauth2/redirect/google", passport.authenticate('auth-google', { session: false }), (req, res) => {
+    jwt.sign({ user: req.user }, "secretKey", { expiresIn: "1h" }, (err, token) => {
+        if (err) {
+            return res.json({
+                token: null,
+        })};
+        res.json({
+            token,
+        })}
+)});
 
 module.exports = router
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,22 +1,52 @@
 const { ExtractJwt, Strategy } = require('passport-jwt')
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const passport = require('passport')//la seguridad autenticacion y verificacion
+const { findUserById, findUserByEmail } = require('../users/users.controllers')
+const config = require('../../config').api
+const userControllers = require('../users/users.controllers')
+require('dotenv').config
 
-const { findUserById } = require('../users/users.controllers')
-const config = require('../../config').api 
-
+const passportGoogleConfig = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/oauth2/redirect/google',
+    passReqToCallback: true,
+}
 const passportConfigs = {
     //Se maneja la estrategia con bearerToken
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: config.secretOrKey //palabra secreta
 }
+
+passport.use('auth-google', new GoogleStrategy(passportGoogleConfig, (request, accessToken, refreshToken, profile, done) => {
+    findUserByEmail(profile.emails[0].value)
+        .then(data => {
+            if (data) {
+                return done(null, data)
+            } else {
+                const newUser = userControllers.createNewUser({
+                    "firstName" : profile.displayName,
+                    "email" : profile.emails[0].value,
+                    "password" : "temporal",
+                    "country" : "countryTemporal",
+                    "Biography" : "biography"
+                })
+                return done(null, newUser)
+            }
+        })
+        .catch(err => {
+            return done(err, false)
+        })
+}));
+
 //*New estrategia...el token lo decodifica 
-passport.use(new Strategy(passportConfigs, (tokenDecoded, done) => {
+passport.use('jwt', new Strategy(passportConfigs, (tokenDecoded, done) => {
     findUserById(tokenDecoded.id)
         .then(data => {
-            if(data){
-               done(null, tokenDecoded) //? El usuario si Existe y es valido
+            if (data) {
+                done(null, tokenDecoded) //? El usuario si Existe y es valido
             } else {
-               done(null, false, {message: 'Token Incorrect'}) //? El usuario no existe
+                done(null, false, { message: 'Token Incorrect' }) //? El usuario no existe
             }
         })
         .catch(err => {
@@ -24,4 +54,4 @@ passport.use(new Strategy(passportConfigs, (tokenDecoded, done) => {
         })
 }))
 
-module.exports = passport.authenticate('jwt', {session: false})
+module.exports = passport
