380300e6b760f3dd1d7395dccc430c244dea2c1f
arbelais <arbelaism@outlook.com>
2023-04-01 18:31:46 -0300

    feat: add react-query and formik

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import MainLayout from 'components/Layout'
 import { About, CompleteProfile, Home, Login, Register } from 'pages'
 import { Route, Routes } from 'react-router-dom'
 
+const queryClient = new QueryClient()
+
 function App(): JSX.Element {
   return (
-    <MainLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/completeprofile" element={<CompleteProfile />} />
-      </Routes>
-    </MainLayout>
+    <QueryClientProvider client={queryClient}>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/completeprofile" element={<CompleteProfile />} />
+        </Routes>
+      </MainLayout>
+    </QueryClientProvider>
   )
 }
 
