09fc2b978d9d6a73a7c8a67afbd7701859f9c92f
Damian Barera <damian_g1988@hotmail.com>
2023-04-04 16:25:20 -0300

    feat: fixed responsive menu
    changed line 13 in navbar-index.tsx

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,42 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
-const Navbar = (): JSX.Element => {
-  return (
-    <nav className="w-full px-16 py-6 flex justify-between items-center shadow-sm bg-white h-[8vh]">
-      <Link to="/" className="font-bold text-xl uppercase">
-        Logo
-      </Link>
-      <div className="flex gap-12">
-        <ul className="flex justify-center items-center gap-12">
-          <li className="text-base cursor-pointer">
-            <Link to="#">¿Cómo funciona?</Link>
-          </li>
-          <li className="text-base cursor-pointer">
-            <Link to="/about">Nosotros</Link>
-          </li>
-          <li className="text-base cursor-pointer">
-            <Link to="/register">Comenzá</Link>
-          </li>
-          <li className="text-base cursor-pointer">
-            <Link to="#">Contactanos</Link>
-          </li>
-          <li className="text-base cursor-pointer">
-            <Link to="#">Contacto</Link>
-          </li>
-        </ul>
-        <Link
-          to="/login"
-          className="flex items-center justify-center bg-gray-200 text-center font-medium shadow-lg rounded-3xl uppercase w-28 h-12 2xl:text-xl 2xl:w-36 2xl:h-16">
-          Login
-        </Link>
-      </div>
-    </nav>
-  )
-}
-
-export default Navbar
-
 const NavbarResponsive = (): JSX.Element => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
   const toggleMenu = (): void => {
@@ -46,7 +10,7 @@ const NavbarResponsive = (): JSX.Element => {
     isMenuOpen ? '' : 'hidden'
   }`
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-[#fb8500] p-6 h-[8vh]">
+    <nav className="flex items-center justify-between flex-wrap bg-[#fb8500] p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link to="/" className="font-semibold text-xl tracking-tight uppercase">
           POWER PLAY
@@ -100,4 +64,4 @@ const NavbarResponsive = (): JSX.Element => {
   )
 }
 
-export { NavbarResponsive }
+export default NavbarResponsive
