be1dcf2cc7b96e807234899593d645c89950bf4f
yonroa <yonanderson11@gmail.com>
2023-04-04 14:07:53 -0500

    Feat: Se agrega inicio de sesion con Facebook

--- backend/src/auth/auth.router.js
+++ backend/src/auth/auth.router.js
@@ -5,10 +5,17 @@ const authServices = require('./auth.services')
 //*Ruta para cuando el usuario se loguea normalmente
 router.post('/login', authServices.postLogin)
 
+//*Ruta para loguearse con Facebook
+router.get("/facebook", passport.authenticate('auth-facebook'))
+
+//*Ruta que llega despues de loguearse en facebook
+router.get("/oauth2/redirect/facebook", passport.authenticate('auth-facebook', { session: false }), authServices.postGoogle)
+
 //*Ruta para loguearse con google
 router.get("/google", passport.authenticate('auth-google', { scope: ["email", "profile"] }))
 
 //*Ruta que llega despues de loguearse en google
 router.get("/oauth2/redirect/google", passport.authenticate('auth-google', { session: false }), authServices.postGoogle)
+
 
 module.exports = router
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,12 +1,23 @@
 const { ExtractJwt, Strategy } = require('passport-jwt')
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require('passport')//la seguridad autenticacion y verificacion
 const { findUserById, findUserByEmail } = require('../users/users.controllers')
 const config = require('../../config').api
 const userControllers = require('../users/users.controllers')
 require('dotenv').config
 
-//*Configuracion para la estrategia de Google
+
+//? Configuracion para la estrategia de Facebook
+const passportFacebookConfig = {
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/api/v1/auth/oauth2/redirect/facebook',
+    passReqToCallback: true,
+    profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
+}
+
+//? Configuracion para la estrategia de Google
 const passportGoogleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -20,6 +31,28 @@ const passportConfigs = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: config.secretOrKey //palabra secreta
 }
+
+//*Estrategia para iniciar sesion con Facebook
+passport.use('auth-facebook', new FacebookStrategy(passportFacebookConfig, (request, accessToken, refreshToken, profile, done) => {
+    findUserByEmail(profile.emails[0].value)
+        .then(data => {
+            if (data) {
+                return done(null, data) //? Si el usuraio se ha logueado antes, solo regresa sus datos
+            } else {
+                const newUser = userControllers.createNewUser({
+                    "firstName" : profile.name['givenName'],
+                    "email" : profile.emails[0].value,
+                    "password" : "temporal",
+                    "country" : "countryTemporal",
+                    "Biography" : "biography"
+                })
+                return done(null, newUser) //? Si el usuario no se ha logueado antes, lo registra en la bd
+            }
+        })
+        .catch(err => {
+            return done(err, false) //? Error en la base de datos
+        })
+}))
 
 //*Estrategia para iniciar sesion con google
 passport.use('auth-google', new GoogleStrategy(passportGoogleConfig, (request, accessToken, refreshToken, profile, done) => {
