a08a04fcad372baec2a97b9fb03481a6268d1bf7
arbelais <arbelaism@outlook.com>
2023-04-04 20:37:48 -0300

    feat(register): add form validation with Yup schema

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,46 +1,40 @@
 import { useMutation } from '@tanstack/react-query'
+import * as Yup from 'yup'
 import type { INewUser } from 'app/types'
-import { useFormik } from 'formik'
-import { useState } from 'react'
+import { Formik, Field, Form } from 'formik'
 import { BsFillArrowLeftCircleFill } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
 import { registerUser } from 'utils'
+import { Tooltip } from 'components'
+
+/* TODO: add minimum length for password.
+ * password: /^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+ *                 ^ este regex es para testear que la contraseña tenga:
+ *                 más de 8 caracteres, una letra mayúscula, una minúscula y un número
+ *                 después vemos como lo manejamos
+ */
+const SignUpSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too short!')
+    .max(50, 'Too Long')
+    .required('Required')
+    .matches(/[a-zA-Z\s:]/, 'Numbers and special characters forbbiden'),
+  email: Yup.string().email('Invalid Email').required('Required'),
+  password: Yup.string().required('Required'),
+  repassword: Yup.string().oneOf([Yup.ref('password')], 'Passwords must match')
+})
 
 const INITIAL_STATE: INewUser = {
   firstName: '',
   email: '',
-  password: ''
+  password: '',
+  repassword: ''
 }
 
 const Register = (): JSX.Element => {
   const navigate = useNavigate()
-  const [user, setUser] = useState<INewUser>(INITIAL_STATE)
 
   const { mutate } = useMutation(registerUser)
-  const formik = useFormik({
-    initialValues: INITIAL_STATE,
-    onSubmit: (values) => {
-      mutate(values)
-    }
-  })
-
-  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-  //   e.preventDefault()
-  //
-  //   setUser({
-  //     ...user,
-  //     [e.target.name]: e.target.value
-  //   })
-  // }
-  //
-  // console.log(user)
-  //
-  // const handleSubmit = (e: React.FormEvent): void => {
-  //   e.preventDefault()
-  //
-  //   mutate(user)
-  // }
-
   return (
     <div className="flex items-center justify-center h-screen w-full">
       <div className="flex flex-col items-center bg-white rounded-xl mx-5 w-[80%] md:max-w-[50%] lg:max-w-[40%]">
@@ -55,70 +49,99 @@ const Register = (): JSX.Element => {
         <div>Imagen</div>
         <div className="my-5">Registrate</div>
 
-        <form
-          onSubmit={formik.handleSubmit}
-          className="flex flex-col gap-4 text-center w-full p-10">
-          <div className="relative">
-            <input
-              type="text"
-              name="firstName"
-              onChange={formik.handleChange}
-              className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
-              placeholder=" "
-            />
-            <label
-              htmlFor="firstName"
-              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-              Ingrese su nombre
-            </label>
-          </div>
-          <div className="relative">
-            <input
-              type="mail"
-              name="email"
-              onChange={formik.handleChange}
-              className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
-              placeholder=" "
-            />
-            <label
-              htmlFor="email"
-              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-              Ingrese su email
-            </label>
-          </div>
-          <div className="relative">
-            <input
-              type="password"
-              name="password"
-              onChange={formik.handleChange}
-              className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
-              placeholder=" "
-            />
-            <label
-              htmlFor="password"
-              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-              Ingrese su contraseña
-            </label>
-          </div>
-          <div className="relative">
-            <input
-              type="password"
-              name="repassword"
-              onChange={formik.handleChange}
-              className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
-              placeholder=" "
-            />
-            <label
-              htmlFor="repassword"
-              className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-              Reingrese su contraseña
-            </label>
-          </div>
+        <Formik
+          initialValues={INITIAL_STATE}
+          validationSchema={SignUpSchema}
+          onSubmit={(values, actions) => {
+            mutate(values)
+            actions.resetForm({ values: INITIAL_STATE })
+          }}>
+          {({ errors, touched }) => (
+            <Form className="flex flex-col text-center gap-4 w-full p-10">
+              <div className="relative">
+                <Field
+                  name="firstName"
+                  className={`block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg  appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer ${
+                    errors.firstName
+                      ? 'border border-red-500'
+                      : 'border border-gray-300'
+                  }`}
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="firstName"
+                  className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+                  Ingrese su nombre
+                </label>
+                {errors.firstName && touched.firstName ? (
+                  <span className="text-red-500 text-xl absolute right-4 top-2/4 -translate-y-2/4">
+                    <Tooltip message={errors.firstName} />
+                  </span>
+                ) : null}
+              </div>
+              <div className="relative">
+                <Field
+                  type="email"
+                  name="email"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="email"
+                  className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+                  Ingrese su email
+                </label>
+                {errors.email && touched.email ? (
+                  <span className="text-red-500 text-xl absolute right-4 top-2/4 -translate-y-2/4">
+                    <Tooltip message={errors.email} />
+                  </span>
+                ) : null}
+              </div>
+              <div className="relative">
+                <Field
+                  type="password"
+                  name="password"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="password"
+                  className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+                  Ingrese su contraseña
+                </label>
+                {errors.password && touched.password ? (
+                  <span className="text-red-500 text-xl absolute right-4 top-2/4 -translate-y-2/4">
+                    <Tooltip message={errors.password} />
+                  </span>
+                ) : null}
+              </div>
+              <div className="relative">
+                <Field
+                  type="password"
+                  name="repassword"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="repassword"
+                  className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+                  Reingrese su contraseña
+                </label>
+                {errors.repassword && touched.repassword ? (
+                  <span className="text-red-500 text-xl absolute right-4 top-2/4 -translate-y-2/4">
+                    <Tooltip message={errors.repassword} />
+                  </span>
+                ) : null}
+              </div>
 
-          <button className="uppercase bg-gray-300 hover:bg-gray-500 hover:text-white ease-in-out duration-300 text-black rounded-md h-10 font-light">
-            Registrarme
-          </button>
-        </form>
+              <button
+                type="submit"
+                className="uppercase bg-gray-300 hover:bg-gray-500 hover:text-white ease-in-out duration-300 text-black rounded-md h-10 font-light">
+                Registrarme
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   )
