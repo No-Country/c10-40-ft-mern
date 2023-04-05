b6d57b8e19123ee8e9fd615e4304e61e50d011e5
Lucas Macias <chueko@gmail.com>
2023-04-05 14:02:09 -0300

    feat: login with facebook

--- backend/src/auth/auth.router.js
+++ backend/src/auth/auth.router.js
@@ -1,14 +1,31 @@
-const router = require('express').Router()
-const passport = require('passport');
-const authServices = require('./auth.services')
+const router = require("express").Router();
+const passport = require("passport");
+const authServices = require("./auth.services");
 
 //*Ruta para cuando el usuario se loguea normalmente
-router.post('/login', authServices.postLogin)
+router.post("/login", authServices.postLogin);
+
+//*Ruta para loguearse con Facebook
+router.get("/facebook", passport.authenticate("auth-facebook"));
+
+//*Ruta que llega despues de loguearse en facebook
+router.get(
+  "/oauth2/redirect/facebook",
+  passport.authenticate("auth-facebook", { session: false }),
+  authServices.postSocial
+);
 
 //*Ruta para loguearse con google
-router.get("/google", passport.authenticate('auth-google', { scope: ["email", "profile"] }))
+router.get(
+  "/google",
+  passport.authenticate("auth-google", { scope: ["email", "profile"] })
+);
 
 //*Ruta que llega despues de loguearse en google
-router.get("/oauth2/redirect/google", passport.authenticate('auth-google', { session: false }), authServices.postGoogle)
+router.get(
+  "/oauth2/redirect/google",
+  passport.authenticate("auth-google", { session: false }),
+  authServices.postSocial
+);
 
-module.exports = router
+module.exports = router;
