97c57dea78c734d0db2892ee056ce0ea23346233
Damian Barera <damian_g1988@hotmail.com>
2023-04-09 22:46:56 -0300

    feat: add Calendar component with subcomponents, util.ts and dayjs library

--- frontend/src/components/Calendar/index.tsx
+++ frontend/src/components/Calendar/index.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+import { getMonth } from 'components/Calendar/CalendarComp/calendarUtils'
+import CalendarHeader from './CalendarComp/CalendarHeader'
+import CalendarSidebar from './CalendarComp/CalendarSidebar'
+import CalendarMonth from './CalendarComp/CalendarMonth'
+
 const Calendar = (): JSX.Element => {
+  const [currentMonth, setCurrentMonth] = useState(getMonth())
   return (
-    <div>
-      <h1>Componente Calendar</h1>
-    </div>
+    <>
+      <div className="w-screen flex flex-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <CalendarSidebar />
+          <CalendarMonth month={currentMonth} />
+        </div>
+      </div>
+    </>
   )
 }
 
