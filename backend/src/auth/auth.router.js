8563935cc06c45d7d78bf3a08a3a6ed72332a991
yonroa <yonanderson11@gmail.com>
2023-04-10 17:44:01 -0500

    Remove: Se quito el servicio de login con facebook

--- backend/src/auth/auth.router.js
+++ backend/src/auth/auth.router.js
@@ -5,22 +5,6 @@ const authServices = require("./auth.services");
 //*Ruta para cuando el usuario se loguea normalmente
 router.post("/login", authServices.postLogin);
 
-//*Ruta para loguearse con Facebook
-router.get("/facebook", passport.authenticate("auth-facebook"));
-
-//*Ruta que llega despues de loguearse en facebook
-router.get(
-  "/oauth2/redirect/facebook",
-  passport.authenticate("auth-facebook", { session: false }),
-  authServices.postSocial
-);
-
-//*Ruta para loguearse con Facebook
-router.get("/facebook", passport.authenticate('auth-facebook'))
-
-//*Ruta que llega despues de loguearse en facebook
-router.get("/oauth2/redirect/facebook", passport.authenticate('auth-facebook', { session: false }), authServices.postGoogle)
-
 //*Ruta para loguearse con google
 router.get(
   "/google",
