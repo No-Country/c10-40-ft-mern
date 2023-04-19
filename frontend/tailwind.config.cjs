14c68c8822c565b2b4c20b151c325167245b97e7
arbelais <arbelaism@outlook.com>
2023-04-18 21:19:15 -0300

    feat: add new fonts

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,10 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      Barlow: ['Barlow Condensed', 'sans-serif'],
+      Biryani: ['Biryani', 'sans-serif']
+    },
     extend: {
-      boxShadow: {
-        xxl: '0 0 0 1000px rgba(0,0,0,0.3)'
-      },
       backgroundImage: {
         hero: "url('https://hips.hearstapps.com/hmg-prod/images/young-strong-sweaty-focused-fit-muscular-man-with-royalty-free-image-1594652842.jpg?resize=1200:*')"
       }
