e9dbf5b5d41564496c1aec622c6ee69067e23b01
matu <salinasmateo@oulook.com>
2023-04-21 16:49:46 -0300

    fix: :bug: bug in routineDays

--- frontend/src/components/RoutineDays2/index.tsx
+++ frontend/src/components/RoutineDays2/index.tsx
@@ -13,17 +13,13 @@ const RoutineDays2 = (): JSX.Element => {
       {isLoading ? (
         <Loader type={'routine'} />
       ) : (
-        <div className="flex flex-col mt-5 max-w-screen-xl">
-          <p className="text-3xl text-center md:text-start w-full mx-auto my-6">
-            Rutinas creadas para ti
-          </p>
+        <div className="flex flex-col mt-5 max-w-screen-lg">
           {data?.[2].days.map((dia3: any) => {
             const [showRut, setShowRut] = useState(false)
-
             return (
               <div className="flex flex-col " key={dia3.id}>
-                <div className="flex items-center md:justify-center mx-5 w-72">
-                  <p className="font-semibold text-2xl py-5 w-full">
+                <div className="flex items-center md:justify-center max-w-lg">
+                  <p className="font-semibold text-2xl mx-5 py-5">
                     Día {dia3.day}
                   </p>
                   {showRut ? (
@@ -45,7 +41,7 @@ const RoutineDays2 = (): JSX.Element => {
                   )}
                 </div>
                 {showRut && (
-                  <div className="grid grid-cols-1 w-full md:grid-cols-2 xl:grid-cols-3 md:mx-auto justify-center gap-5">
+                  <div className="grid grid-cols-1 w-full md:grid-cols-2 lg:grid-cols-3 md:mx-auto justify-center gap-5">
                     {dia3?.exercises.map((ex: any) => {
                       return (
                         <ExerciseCard
--- frontend/src/components/RoutineDays5/index.tsx
+++ frontend/src/components/RoutineDays5/index.tsx
@@ -18,8 +18,8 @@ const RoutineDays5 = (): JSX.Element => {
 
             return (
               <div className="flex flex-col " key={dia3.id}>
-                <div className="flex items-center md:justify-center mx-5 w-72">
-                  <p className="font-semibold text-2xl py-5 w-full">
+                <div className="flex items-center md:justify-center max-w-lg">
+                  <p className="font-semibold text-2xl mx-5 py-5">
                     Día {dia3.day}
                   </p>
                   {showRut ? (
@@ -41,7 +41,7 @@ const RoutineDays5 = (): JSX.Element => {
                   )}
                 </div>
                 {showRut && (
-                  <div className="grid grid-cols-1 w-full md:grid-cols-2 xl:grid-cols-3 md:mx-auto justify-center gap-5">
+                  <div className="grid grid-cols-1 w-full md:grid-cols-2 lg:grid-cols-3 md:mx-auto justify-center gap-5">
                     {dia3?.exercises.map((ex: any) => {
                       return (
                         <ExerciseCard
