31a80c5f29d7a6d737741bf4140b59e3452038f0
arbelais <arbelaism@outlook.com>
2023-04-20 22:41:35 -0300

    fix: modal in register

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -2,13 +2,14 @@ import { useMutation } from '@tanstack/react-query'
 import * as Yup from 'yup'
 import type { INewUser } from 'app/types'
 import { Formik, Field, Form } from 'formik'
-import { BsFillArrowLeftCircleFill } from 'react-icons/bs'
 import { Link, useNavigate } from 'react-router-dom'
 import { registerUser } from 'utils'
 import { Tooltip, MyModal } from 'components'
-import { useUser } from 'hooks/useUser'
 import { useEffect, useState } from 'react'
 import { ImSpinner8 } from 'react-icons/im'
+import { sendNotification } from 'utils/sendNotification'
+import { isAxiosError } from 'axios'
+import { useAuth } from 'hooks/useAuth'
 
 /* TODO: add minimum length for password.
  * password: /^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
@@ -38,19 +39,34 @@ const INITIAL_STATE: INewUser = {
 
 const Register = (): JSX.Element => {
   const navigate = useNavigate()
-  const userQuery = useUser()
   const [modal, setModal] = useState(false)
+  const { isAuthenticated, isLoading } = useAuth()
 
   useEffect(() => {
-    if (!userQuery.isLoading && userQuery.data) {
+    if (!isLoading && isAuthenticated) {
       navigate('/dashboard')
     }
-  }, [userQuery.data])
+  }, [isLoading, isAuthenticated])
 
-  const { mutate, isLoading, error } = useMutation({
+  const {
+    mutate,
+    isLoading: mutationIsLoading,
+    error
+  } = useMutation({
     mutationFn: registerUser,
     onSuccess: () => {
+      sendNotification('Registro realizado con exito.', 'success')
       navigate('/login')
+    },
+    onError: (error) => {
+      if (isAxiosError(error)) {
+        if (error.response?.status === 400) {
+          sendNotification(
+            'Ya hay una cuenta con esa dirección de email.',
+            'error'
+          )
+        }
+      }
     }
   })
 
@@ -63,11 +79,10 @@ const Register = (): JSX.Element => {
         <Formik
           initialValues={INITIAL_STATE}
           validationSchema={SignUpSchema}
-          onSubmit={(values, actions) => {
+          onSubmit={(values) => {
             mutate(values)
-            actions.resetForm({ values: INITIAL_STATE })
           }}>
-          {({ errors, touched }) => (
+          {({ values, errors, touched, setFieldValue }) => (
             <Form className="flex flex-col gap-5 text-center w-full py-6 lg:py-10 lg:px-10">
               <div className="relative">
                 <Field
@@ -173,60 +188,61 @@ const Register = (): JSX.Element => {
                 ) : null}
               </div>
               {/* Terms  */}
-              <div className="relative">
-                <Field
-                  type="checkbox"
-                  name="terms"
-                  className={`rounded-md font-semibold ease-in duration-200 flex mb-8 items-center w-3 h-3 ${
-                    errors.terms && touched.terms
-                      ? 'text-secondary-400'
-                      : 'text-primary-100/70'
-                  }`}
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="terms"
-                  className="flex absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-3 z-10 px-0 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-                  Acepto los{' '}
-                  <p
-                    onClick={() => {
-                      setModal(true)
-                    }}
-                    className="pl-1 text-blue-600 hover:underline">
-                    términos y condiciones
-                  </p>
-                </label>
+              <div className="flex flex-col justify-between gap-2">
+                <div className="flex items-center gap-2">
+                  <Field
+                    type="checkbox"
+                    name="terms"
+                    id="terms"
+                    className="w-4 h-4 text-primary-600 bg-primary-100 border-primary-300 rounded "
+                    placeholder=" "
+                  />
+                  <label
+                    htmlFor="terms"
+                    className="flex text-sm items-center gap-2 text-gray-600">
+                    Acepto los
+                    <button
+                      onClick={() => {
+                        setModal(true)
+                      }}
+                      className="text-primary-400/80 hover:text-primary-400 ease-in duration-300">
+                      {' '}
+                      términos y condiciones
+                    </button>
+                  </label>
+                </div>
                 {errors.terms && touched.terms ? (
-                  <span className="text-red-500 text-xl absolute right-4 top-2/4 -translate-y-2/4">
+                  <span className="text-secondary-400 text-xl self-end">
                     <Tooltip message={errors.terms} />
                   </span>
                 ) : null}
               </div>
               <button
                 type="submit"
-                disabled={isLoading}
+                disabled={mutationIsLoading}
                 className="flex items-center justify-center uppercase font-Barlow font-semibold py-2.5 lg:py-3 text-lg lg:text-xl bg-primary-400/60 hover:bg-primary-400 hover:text-primary-bg ease-in-out duration-300 text-primary-bg rounded-md">
-                {isLoading ? (
+                {mutationIsLoading ? (
                   <ImSpinner8 className="animate-spin" />
                 ) : (
                   'Registrate'
                 )}
               </button>
 
-              {error ? <p className="text-red-500">Hubo un error</p> : null}
-              <div className="flex items-center justify-center my-4 gap-2">
+              <div className="flex items-center justify-center text-sm lg:text-base pt-5 lg:pt-4 gap-2">
                 <span>¿Ya tenés cuenta?</span>
                 <Link
                   to="/login"
-                  className=" hover:text-primary-400 hover:border-primary-400 hover:scale-105 ease-in duration-300">
-                  Ingresa
+                  className="text-primary-400/80 hover:text-primary-400 ease-in duration-300">
+                  Ingresar
                 </Link>
               </div>
+              {modal && (
+                <MyModal setModal={setModal} setFieldValue={setFieldValue} />
+              )}
             </Form>
           )}
         </Formik>
       </div>
-      {modal && <MyModal setModal={setModal} />}
     </div>
   )
 }
