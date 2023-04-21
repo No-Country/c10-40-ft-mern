266a0c81f4eca67a0a894ea9a58180efa9c5a62f
matu <salinasmateo@oulook.com>
2023-04-21 16:21:31 -0300

    style: :art: change cards for routines

--- frontend/src/components/RoutineDays3/index.tsx
+++ frontend/src/components/RoutineDays3/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import { useRoutine } from 'hooks/useRoutine'
 import ExerciseCard from 'components/ExerciseCard'
 import Loader from 'components/Loader'
+import { AiOutlineClose } from 'react-icons/ai'
+import { IoIosArrowDown } from 'react-icons/io'
 
 const RoutineDays3 = (): JSX.Element => {
   const { data, isLoading } = useRoutine()
@@ -11,18 +14,45 @@ const RoutineDays3 = (): JSX.Element => {
       ) : (
         <div className="flex flex-col">
           {data?.[1].days.map((dia3: any) => {
+            const [showRut, setShowRut] = useState(false)
+
             return (
-              <div className="flex flex-col" key={dia3.id}>
-                <h3 className="text-center font-bold">Día {dia3.day}</h3>
-                <div className="flex justify-center">
-                  {dia3?.exercises.map((ex: any) => {
-                    return (
-                      <div className="" key={ex.id}>
-                        <ExerciseCard isLoading={isLoading} exercises={ex} />
-                      </div>
-                    )
-                  })}
+              <div className="flex flex-col " key={dia3.id}>
+                <div className="flex items-center md:justify-center mx-5 w-72">
+                  <p className="font-semibold text-2xl py-5 w-full">
+                    Día {dia3.day}
+                  </p>
+                  {showRut ? (
+                    <AiOutlineClose
+                      className="text-primary-400 ml-5"
+                      size={24}
+                      onClick={() => {
+                        setShowRut(!showRut)
+                      }}
+                    />
+                  ) : (
+                    <IoIosArrowDown
+                      className="text-primary-400 ml-5"
+                      size={30}
+                      onClick={() => {
+                        setShowRut(!showRut)
+                      }}
+                    />
+                  )}
                 </div>
+                {showRut && (
+                  <div className="grid grid-cols-1 w-full md:grid-cols-2 xl:grid-cols-3 md:mx-auto justify-center gap-5">
+                    {dia3?.exercises.map((ex: any) => {
+                      return (
+                        <ExerciseCard
+                          isLoading={isLoading}
+                          exercises={ex}
+                          key={ex.id}
+                        />
+                      )
+                    })}
+                  </div>
+                )}
               </div>
             )
           })}
